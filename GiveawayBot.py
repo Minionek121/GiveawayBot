5baeeebf97a9293e84e269e3863bf479b68b4f2b
Minionek121 <157628625+Minionek121@users.noreply.github.com>
2026-06-12 23:21:09 +0200

    Update GiveawayBot.py

--- GiveawayBot.py
+++ GiveawayBot.py
@@ -1667,7 +1667,7 @@ async def end_giveaway(message_id, reroll=False):
     weighted = []
     for user in users:
         lvl = await get_level(channel.guild.id, user.id)
-        weighted.extend([user] * random.randint(1, max(1, lvl // 4))
+        weighted.extend([user] * random.randint(1, max(1, lvl // 4)))
 
     winners = []
     while len(winners) < min(winner_count, len(users)) and weighted:
